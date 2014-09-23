19cfbbb35c960f57f9f627cd1da62fab7aa0728e
Travis Yoder <trayoda@gmail.com>
2014-09-23 11:55:58 -0600

    Fixed some wording to make more sense what is being tested

--- tests/code_maker_test.rb
+++ tests/code_maker_test.rb
@@ -9,24 +9,24 @@ require '../lib/code_maker'
 
 class CodeMakerTest < Minitest::Test
 
-  def test_code_is_four_characters_long
+  def test_new_code_is_four_characters_long
     code_maker = CodeMaker.new
     assert_equal 4, code_maker.code.length
   end
 
-  def test_code_is_only_red_blue_green_or_yellow
+  def test_new_code_is_only_red_blue_green_or_yellow
     code_maker = CodeMaker.new
     code_maker.code.chars.each do |color|
       assert ['r', 'g', 'y', 'b'].include? color
     end
   end
 
-  def test_uses_specific_code
+  def test_accepts_specific_code
     code_maker = CodeMaker.new("rrrr")
     assert "rrrr", code_maker.code
   end
 
-  def test_it_makes_random_code
+  def test_it_makes_a_random_code
     code_maker1 = CodeMaker.new
     code_maker2 = CodeMaker.new
     refute code_maker1.code == code_maker2.code
