e89e83be9ac478f18366baa4818e958d83ba7b3d
Travis Yoder <trayoda@gmail.com>
2014-09-24 16:46:10 -0600

    Changed where welcome message prints and added a comment on what the win_condition is

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,23 +1,27 @@
-# *import colorize*
-
 class CLI
 
   attr_reader :user_input, :printer
 
   def initialize
-    @user_input = ""
+    @user_input = InputGetter.new
     @printer = Printer.new
   end
 
   def start
-
-    printer.startup
     until quit?
+      printer.welcome_message
       printer.enter_command
-      @user_input = gets.strip
+      @user_input.get
+      @user_input.input
       case
       when play?
-        printer.play
+        game = Game.new(@user_input)
+        game.play
+      when cheat_code?
+        printer.enter_cheat_code
+        cheat_code = gets.strip
+        game = Game.new(@user_input, CodeMaker.new(cheat_code))
+        game.play
       when instructions?
         printer.instructions
       when quit?
@@ -33,15 +37,19 @@ class CLI
   private
 
   def play?
-    user_input == 'p' || user_input == 'play'
+    @user_input.input == 'p' || @user_input.input == 'play'
+  end
+
+  def cheat_code?
+    @user_input.input == 'uuddlrlrba'
   end
 
   def instructions?
-    user_input == 'i' || user_input == 'instructions'
+    @user_input.input == 'i' || @user_input.input == 'instructions'
   end
 
   def quit?
-    user_input == 'q' || user_input == 'quit'
+    @user_input.input == 'q' || @user_input.input == 'quit'
   end
 
 end
---
+++ lib/game.rb
@@ -0,0 +1,53 @@
+class Game
+
+  attr_reader :user_input,
+              :printer,
+              :code_maker,
+              :guesses,
+              :win_condition
+
+  def initialize(input_getter_from_CLI, code_maker = nil)
+    @printer = Printer.new
+    @code_maker = code_maker || CodeMaker.new
+    @guesses = []
+    @user_input = input_getter_from_CLI
+    # The win_condition array evaluates to
+    # [correct_positions, correct_colors]
+    @win_condition = [4, 0]
+  end
+
+  def play
+    printer.play
+    until win? || quit?
+      printer.make_guess
+      @user_input.get
+      case
+      when quit?
+        printer.quit
+      when @user_input.input.length != 4
+        printer.invalid_input
+      when win?
+        printer.winner
+      else
+        guesses << @user_input.input
+        results_array = GuessChecker.compare(code_maker.code, @user_input.input)
+        printer.response(@user_input.input, results_array, guesses.length)
+      end
+
+    end
+
+  end
+
+
+  private
+
+  def win?
+    results_array = GuessChecker.compare(code_maker.code, @user_input.input)
+    results_array == win_condition
+  end
+
+  def quit?
+    @user_input.input == 'q' || @user_input.input == 'quit'
+  end
+
+end
