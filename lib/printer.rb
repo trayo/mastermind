d5d208386c9999bd6da9fe0ac7dcfe728783ae7e
Travis Yoder <trayoda@gmail.com>
2014-09-24 18:48:53 -0600

    Refactored printer and implemented printer_test

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,44 +1,54 @@
-# display instructions
-# display score with time
-# display intro screen
-# display when user gets answer right
-# display when user gets answer wrong
-# display when game ends
-
-
 class Printer
 
-  def startup
-    puts "\nWelcome to MASTERMIND"
+  def initialize(stdout)
+    @stdout = stdout
+  end
+
+  def welcome_message
+    @stdout.puts "\nWelcome to MASTERMIND"
     start_screen
   end
 
-  def enter_command
-    print "\nEnter a command: "
+  def start_screen
+    @stdout.puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
   end
 
-  def start_screen
-    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  def enter_command
+    @stdout.print "\nEnter a command (Ex:'p' for play): "
   end
 
   def instructions
-    puts "Here lies Instructions, may he rest in peace."
+    @stdout.puts "Here lies Instructions, may he rest in peace."
+  end
+
+  def make_guess
+    @stdout.print "\nMake a guess (Ex:'rrrr'): "
   end
 
   def invalid_input
-    puts "Invalid input."
+    @stdout.puts "Invalid input. Please try again."
   end
 
   def quit
-    puts "Good bye!"
+    @stdout.puts "No! Don't go!"
   end
 
   def play
-    puts "Thanks for playing."
+    @stdout.puts "I have generated a beginner sequence with four elements made up of: " +
+                 "(r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
   end
 
-  # def win
-  #
-  # end
+  def response(input, results, guesses)
+    @stdout.puts "'#{input}' has #{results[1]} of the correct colors with #{results[0]} in the correct positions."
+    @stdout.puts "You've taken #{guesses} guesses."
+  end
+
+  def winner
+    @stdout.puts "You win! You guessed the correct code '#{code}' in #{guesses} guesses."
+  end
+
+  def enter_cheat_code
+    @stdout.print "Enter cheat code: "
+  end
 
 end
--- tests/printer_test.rb
+++ tests/printer_test.rb
@@ -1,16 +1,66 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/printer'
+require 'stringio'
+require_relative '../lib/printer'
 
 class PrinterTest < Minitest::Test
 
-  def test_startup_message
-    assert_output "Welcome to MASTERMIND",{ Printer.startup}
+  attr_reader :printer, :mock_stdout
+
+  def setup
+    @mock_stdout = StringIO.new
+    @printer = Printer.new(mock_stdout)
+  end
+
+  def test_welcome_message
+    printer.welcome_message
+    mock_stdout.string.include?('Welcome to MASTERMIND')
   end
 
   def test_start_screen_message
-    assert_equal "Would you like to (p)lay, read the (i)nstructions, or (q)uit?", Printer.start_screen
+    printer.welcome_message
+    mock_stdout.string.include?('Would you like to (p)lay, read the (i)nstructions, or (q)uit?')
+  end
+
+  def test_enter_command
+    printer.welcome_message
+    mock_stdout.string.include?("Enter a command (Ex:'p' for play): ")
+  end
+
+  def test_instructions
+    printer.welcome_message
+    mock_stdout.string.include?('Here lies Instructions, may he rest in peace.')
+  end
+
+  def test_make_guess
+    printer.welcome_message
+    mock_stdout.string.include?("Make a guess (Ex:'rrrr'): ")
+  end
+
+  def test_invalid_input
+    printer.welcome_message
+    mock_stdout.string.include?("Invalid input. Please try again.")
+  end
+
+  def test_quit
+    printer.welcome_message
+    mock_stdout.string.include?("No! Don't go!")
+  end
+
+  def test_response
+    printer.welcome_message
+    mock_stdout.string.include?('Welcome to MASTERMIND')
+  end
+
+  def test_winner
+    printer.welcome_message
+    mock_stdout.string.include?("You win! You guessed the correct code")
+  end
+
+  def test_enter_cheat_code
+    printer.welcome_message
+    mock_stdout.string.include?('Enter cheat code: ')
   end
 
 end
