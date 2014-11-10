669a112be54c3a486f33fcba0ef2eda869a6a5c5
Travis Yoder <trayoda@gmail.com>
2014-11-10 15:50:20 -0700

    refacorted test

--- tests/code_maker_test.rb
+++ tests/code_maker_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/code_maker'
+require_relative '../lib/code_maker'
 
 class CodeMakerTest < Minitest::Test
 
@@ -13,8 +13,7 @@ class CodeMakerTest < Minitest::Test
   def test_new_code_is_only_red_blue_green_or_yellow
     code_maker = CodeMaker.new
     code_maker.code.chars.each do |color|
-    # assert ['r', 'g', 'y', 'b'].include? color
-    assert %W(r g b y).include? color
+      assert %W(r g b y).include? color
     end
   end
 
